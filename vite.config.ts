16dc19f709846fa64a143d9cb61ece947e0ac937
Seaman Hou <hhool.student@sina.com>
2025-02-22 22:58:57 +0800

    temp

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-plugins: [
-  react(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: 'public/images/*.jpg', // Adjust the path to your JSON files
-        dest: 'images/' // Copy to the root of the dist directory
-      },
-      {
-        src: 'public/images/*.ico', // Adjust the path to your JSON files
-        dest: 'images/' // Copy to the root of the dist directory
-      }
-    ]
-  })
-],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/images/*.jpg", // Adjust the path to your JSON files
+          dest: "images/", // Copy to the root of the dist directory
+        },
+        {
+          src: "public/images/*.ico", // Adjust the path to your JSON files
+          dest: "images/", // Copy to the root of the dist directory
+        },
+      ],
+    }),
+  ],
+  build: {
+    outDir: "dist",
+  },
+});
